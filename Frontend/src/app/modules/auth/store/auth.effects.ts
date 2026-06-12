339d777bf528699c1db31333f956f5c709f9f5d9
bhavanapawar-cpu <147172702+bhavanapawar-cpu@users.noreply.github.com>
2026-06-12 15:47:47 +0530

    feat: Add NgRx auth effects

---
+++ Frontend/src/app/modules/auth/store/auth.effects.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import * as AuthActions from './auth.actions';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthEffects {
+  login$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.login),
+      switchMap(({ email, password }) =>
+        this.authService.login(email, password).pipe(
+          map(user => AuthActions.loginSuccess({ user })),
+          catchError(error => of(AuthActions.loginFailure({ error: error.error.message })))
+        )
+      )
+    )
+  );
+
+  loginSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.loginSuccess),
+        tap(() => this.router.navigate(['/dashboard']))
+      ),
+    { dispatch: false }
+  );
+
+  register$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.register),
+      switchMap(({ firstName, lastName, email, password, phoneNumber, role }) =>
+        this.authService.register(firstName, lastName, email, password, phoneNumber, role).pipe(
+          map(user => AuthActions.registerSuccess({ user })),
+          catchError(error => of(AuthActions.registerFailure({ error: error.error.message })))
+        )
+      )
+    )
+  );
+
+  registerSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.registerSuccess),
+        tap(() => this.router.navigate(['/dashboard']))
+      ),
+    { dispatch: false }
+  );
+
+  logout$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.logout),
+        tap(() => {
+          this.authService.logout();
+          this.router.navigate(['/auth/login']);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+}
